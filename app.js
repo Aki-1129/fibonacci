4091a382d69d06040535f2408002ae602705324e
Aki <n.aki20331@gmail.com>
2019-08-12 11:33:50 +0000

    指数オーダーのfib関数の実装

--- app.js
+++ app.js
@@ -1,1 +1,13 @@
 'use strict';
+function fib (n) {
+    if (n === 0 ) {
+        return 0;
+    } else if (n === 1) {
+        return 1;
+    }
+    return fib(n - 1) + fib(n - 2);  //一個前の数と二個前の数を足す
+}
+const length = 40;
+for (let i = 0; i <= length; i++) {
+    console.log(fib(i));
+}
